1052bace1e9c1387269cb9c0da66c2d48ad145a3
Beomi <latheledusjp@gmail.com>
2018-09-13 00:31:35 +0900

    update

---
+++ README.md
@@ -0,0 +1,25 @@
+# Naver news contents crawler
+
+## Installation
+
+This project uses `pipenv` to manage dependencies.
+
+> Pipenv can be installed with `pip` or `brew`, etc. Check latest pipenv docs.[https://pipenv.readthedocs.io/en/latest/](https://pipenv.readthedocs.io/en/latest/)
+
+## Features
+
+- Downloads selected date range of news. 
+- 
+
+## Usageㅁ
+
+### Local usages
+
+
+
+### (Recommend) Serverless crawling 
+
+> Need docker to build or Follow [Lambda Packaging at Google colab]()
+
+
+
--- crawler.py
+++ crawler.py
@@ -1,7 +1,12 @@
+import os
 from pprint import pprint
 import json
 import requests
 from bs4 import BeautifulSoup as bs
+
+import boto3
+
+s3 = boto3.client('s3')
 
 headers = {
     'authority': 'news.naver.com',
@@ -39,12 +44,23 @@ def get_news(oid, aid):
         created_at = soup.select_one(
             'div.sponsor > span.t11').text
         updated_at = created_at
-    return dict(
+
+    result = dict(
         title=title,
         created_at=created_at,
         updated_at=updated_at,
-        contents=contents,
+        contents=contents.strip(),
     )
+
+    year, month, day = created_at.split(' ')[0].split('-')
+
+    response = s3.put_object(
+        Body=json.dumps(result),
+        Bucket=os.environ['S3_BUCKET'],
+        Key=f'json/{year}/{month}/{day}/{oid}/{aid}.json',
+    )
+
+    return result
 
 
 if __name__ == '__main__':
